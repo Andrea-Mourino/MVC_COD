4ab89374ede4b528b8e6253b4baf0339638c1863
Andrea Mourino <amourinodesousa@danielcastelao.org>
2025-04-25 08:43:42 +0200

    tests

--- src/TEST.java
+++ src/TEST.java
@@ -6,12 +6,9 @@ class TEST {
     void cambiarVelocidadShouldThrowExceptionIfCocheDoesNotExist() {
     }
 
-
     @Test
     void getCoche() {
     }
-
-
 
     @Test
     void getVelocidadShouldReturnCorrectVelocidad() {
